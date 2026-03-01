16f59db7174c5a248754ba8d8a8d5c0262ca8a7e
datatomas <tomsuarezx@gmail.com>
2026-03-01 17:53:01 -0500

    Feature: added the logo to the top of the website

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,16 @@
 import streamlit as st
 import os
+from pathlib import Path
+import pandas as pd 
+# use this so it does not lose context
+HERE = Path(__file__).resolve().parent
+img_path = HERE / "static" / "qualityxv2.png"   # or qualityx.jpg
+img_path_1 = HERE / "static" / "qualityx.png"   # or qualityx.jpg
+
 
 st.title("Qualityx AI" )
+st.image(str(img_path), caption="Call center analytics in action")
+
 st.header("Analytics for call centers of the future")
 st.subheader("What's a call center without analytics? ")
 st.markdown(" This is the AI _future_ of call centers, and we're here to help you *get there*. ")
@@ -12,4 +21,7 @@ def greet(name):
 """
 st.code(code, language="python")
 st.divider()
-st.image(os.path.join(os.path.join(os.getcwd(),"static","call_center.jpg")), caption="Call center analytics in action")
+
+
+
+st.image(str(img_path_1), caption="Call center analytics in action")
